421652de762f573f8529afe0e6351992a30a1b7e
Diego Hernandez <diegofootball19@gmail.com>
2022-09-14 14:22:58 -0500

    added new soluton TODO: ask about it

--- src/main/java/edu/ust/cisc/CiscArrayList.java
+++ src/main/java/edu/ust/cisc/CiscArrayList.java
@@ -5,11 +5,21 @@ public class CiscArrayList<E> implements CiscList<E> {
     private int size = 0;
     private Object E[];
 
-   public CiscArrayList(){
-    elements = new Object [DEFAULT_CAPACITY];
+    CiscArrayList(){
+    arr = new Object [DEFAULT_CAPACITY];
     }
-    public CiscArrayList(){
-        elements = new Object [DEFAULT_CAPACITY];
+     public int size(){
+        return size;
     }
-
+    public boolean isEmpty() {
+        return size == 0;
+    }
+  public int indexOf(Object o){
+    for(int i = 0; i < size; i++){
+        if(o == arr[i]){
+            return i;
+        }
+    }
+    return -1;
+  }
 }
