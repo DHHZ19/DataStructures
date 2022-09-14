070ec63d65de1d7153ec0706bdf8d4d90d232b43
Diego Hernandez <diegofootball19@gmail.com>
2022-09-14 13:22:31 -0500

    added new variables and constructor

--- src/main/java/edu/ust/cisc/CiscArrayList.java
+++ src/main/java/edu/ust/cisc/CiscArrayList.java
@@ -1,5 +1,15 @@
 package edu.ust.cisc;
 
 public class CiscArrayList<E> implements CiscList<E> {
+    private int DEFAULT_CAPACITY = 10;
+    private int size = 0;
+    private Object E[];
+
+   public CiscArrayList(){
+    elements = new Object [DEFAULT_CAPACITY];
+    }
+    public CiscArrayList(){
+        elements = new Object [DEFAULT_CAPACITY];
+    }
 
 }
